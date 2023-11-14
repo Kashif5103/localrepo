3240008271de472c51f012efad3b76bca016f0f3
Malik Kashif <kaptaan813@gmail.com>
2023-11-15 02:58:33 +0500

    Flutter Coading

---
+++ android/app/src/main/kotlin/com/example/kashif_gee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kashif_gee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('App'),
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            Text('Muhammad Kashif'),
+            Image(
+              image: AssetImage('db.png'),
+            ),
+          ],
+        ),
+      ),
+    ),
+  ));
+}
